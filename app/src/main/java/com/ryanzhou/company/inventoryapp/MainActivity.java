5b1a5c7e5e7cd2b76ae9afa93399c587d19757ef
Ryan Zhou <ryanzhou7@gmail.com>
2016-06-26 23:51:24 -0700

    added FAB

--- app/src/main/java/com/ryanzhou/company/inventoryapp/MainActivity.java
+++ app/src/main/java/com/ryanzhou/company/inventoryapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.ryanzhou.company.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    CoordinatorLayout coordinatorLayout;
+    FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton_add_item);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(coordinatorLayout, "Saved ", Snackbar.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
